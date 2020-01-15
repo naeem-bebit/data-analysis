25ec500b0e4ee42b0692ca0827b26fb44d156141
naeem_hussien <naeem.hussien@bebit.com>
2020-01-15 12:15:04 +0900

    logistic regression

---
+++ logistic-regression/logistic-regression.py
@@ -0,0 +1,28 @@
+# Load libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+
+# Load data with only two classes
+iris = datasets.load_iris()
+X = iris.data[:100,:]
+y = iris.target[:100]
+
+# Standarize features
+scaler = StandardScaler()
+X_std = scaler.fit_transform(X)
+
+# Create logistic regression object
+clf = LogisticRegression(random_state=0)
+
+# Train model
+model = clf.fit(X_std, y)
+
+# Create new observation
+new_observation = [[.5, .5, .5, .5]]
+
+# Predict class
+model.predict(new_observation)
+
+# View predicted probabilities
+model.predict_proba(new_observation)
