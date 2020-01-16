d0120657b36b039b134f3e41a56eebc63b6e0f8c
naeem_hussien <naeem.hussien@bebit.com>
2020-01-16 19:49:13 +0900

    add file

---
+++ linear-regression/linear-regression.py
@@ -0,0 +1,26 @@
+"""Linear regression Boston dataset."""
+# Load libraries
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+import warnings
+
+# Suppress Warning
+warnings.filterwarnings(action="ignore", module="scipy",
+                        message="^internal gelsd")
+
+# Load Boston data (X & Y)
+boston = load_boston()
+X = boston.data
+y = boston.target
+
+# Create linear regression
+regr = LinearRegression()
+
+# Fit the linear regression
+model = regr.fit(X, y)
+
+# View the intercept
+model.intercept_
+
+# View the feature coefficients
+model.coef_
