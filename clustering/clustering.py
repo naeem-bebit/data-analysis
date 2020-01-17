0e45e854098619971d832c5403d36900c5fe9d04
naeem_hussien <naeem.hussien@bebit.com>
2020-01-17 11:11:54 +0900

    add py file

---
+++ clustering/clustering.py
@@ -0,0 +1,31 @@
+"""Clustering using Boston dataset."""
+# Load libraries
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Load data
+iris = datasets.load_iris()
+X = iris.data
+
+# Standarize features
+scaler = StandardScaler()
+X_std = scaler.fit_transform(X)
+
+# Create k-mean object
+clt = KMeans(n_clusters=3, random_state=0, n_jobs=-1)
+
+# Train model
+model = clt.fit(X_std)
+
+# View predict class
+model.labels_
+
+# Create new observation
+new_observation = [[0.8, 0.8, 0.8, 0.8]]
+
+# Predict observation's cluster
+model.predict(new_observation)
+
+# View cluster centers
+model.cluster_centers_
