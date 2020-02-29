e9e4a422bfee27ae8f155444c7e841c6388747f3
naeem_hussien <naeem.hussien@bebit.com>
2020-02-29 22:23:47 +0900

    note

--- python-note/py-note.py
+++ python-note/py-note.py
@@ -5,6 +5,8 @@ n = int(input().strip())
 check = {True: "Not Weird", False: "Weird"}
 print(check[n % 2 == 0 and (n in range(2, 6) or n > 20)])
 
-# integer list of integers combine into a integer
+#  convert a list of integers into a single integer
 for i in range(1, (n + 1)):
     print(i, end="")
+
+int("".join(map(str, range(1, (int(input())+1)))))
