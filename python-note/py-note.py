743808a9f17d6a06e6a1b7908323244507a2c0d9
naeem_hussien <naeem.hussien@bebit.com>
2020-03-09 20:26:53 +0900

    add flask

--- python-note/py-note.py
+++ python-note/py-note.py
@@ -10,3 +10,28 @@ for i in range(1, (n + 1)):
     print(i, end="")
 
 int("".join(map(str, range(1, (int(input())+1)))))
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
