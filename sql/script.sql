7e82debcdf82ef01cf6a83551d6fbb13c4d56464
naeem_hussien <naeem.hussien@bebit.com>
2020-04-04 21:19:40 +0900

    lag examples

--- sql/script.sql
+++ sql/script.sql
@@ -1,4 +1,12 @@
+/* Get the value the column before*/
 SELECT col_1 AS column_one,   
        LAG(col_2, 1,0) OVER (ORDER BY YEAR(col_3)) AS column_two  
 FROM table_one  
-WHERE col_one = 275 and YEAR(col_3) IN ('2005','2006');  
+WHERE col_one = 275 and YEAR(col_3) IN ('2005','2006');
+
+/* Compare values within partitions */
+SELECT TerritoryName, BusinessEntityID, SalesYTD,   
+       LAG (SalesYTD, 1, 0) OVER (PARTITION BY TerritoryName ORDER BY SalesYTD DESC) AS PrevRepSales  
+FROM Sales.vSalesPerson  
+WHERE TerritoryName IN (N'Northwest', N'Canada')   
+ORDER BY TerritoryName;
