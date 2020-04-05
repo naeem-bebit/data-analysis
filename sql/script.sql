3b18b13af9f337b9810a54711db3dc05e8157278
naeem_hussien <naeem.hussien@bebit.com>
2020-04-05 20:33:23 +0900

    add lag sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,3 +1,4 @@
+/* https://docs.microsoft.com/en-us/sql/t-sql/functions/lag-transact-sql?view=sql-server-ver15 */
 /* Get the value the column before*/
 SELECT col_1 AS column_one,   
        LAG(col_2, 1,0) OVER (ORDER BY YEAR(col_3)) AS column_two  
@@ -10,3 +11,33 @@ SELECT TerritoryName, BusinessEntityID, SalesYTD,
 FROM Sales.vSalesPerson  
 WHERE TerritoryName IN (N'Northwest', N'Canada')   
 ORDER BY TerritoryName;
+
+/* Compare value from previous data*/
+SELECT CalendarYear, CalendarQuarter, SalesAmountQuota AS SalesQuota,  
+       LAG(SalesAmountQuota,1,0) OVER (ORDER BY CalendarYear, CalendarQuarter) AS PrevQuota,  
+       SalesAmountQuota - LAG(SalesAmountQuota,1,0) OVER (ORDER BY CalendarYear, CalendarQuarter) AS Diff  
+FROM dbo.FactSalesQuota  
+WHERE EmployeeKey = 272 AND CalendarYear IN (2001, 2002)  
+ORDER BY CalendarYear, CalendarQuarter; 
+
+/* https://docs.microsoft.com/en-us/sql/t-sql/functions/lead-transact-sql?view=sql-server-ver15*/
+/* Compare values between years */
+SELECT BusinessEntityID, YEAR(QuotaDate) AS SalesYear, SalesQuota AS CurrentQuota,   
+    LEAD(SalesQuota, 1,0) OVER (ORDER BY YEAR(QuotaDate)) AS NextQuota  
+FROM Sales.SalesPersonQuotaHistory  
+WHERE BusinessEntityID = 275 and YEAR(QuotaDate) IN ('2005','2006');
+
+/* Compare values within partitions */
+SELECT TerritoryName, BusinessEntityID, SalesYTD,   
+       LEAD (SalesYTD, 1, 0) OVER (PARTITION BY TerritoryName ORDER BY SalesYTD DESC) AS NextRepSales  
+FROM Sales.vSalesPerson  
+WHERE TerritoryName IN (N'Northwest', N'Canada')   
+ORDER BY TerritoryName;
+
+/* Compare values between quarters*/
+SELECT CalendarYear AS Year, CalendarQuarter AS Quarter, SalesAmountQuota AS SalesQuota,  
+       LEAD(SalesAmountQuota,1,0) OVER (ORDER BY CalendarYear, CalendarQuarter) AS NextQuota,  
+   SalesAmountQuota - LEAD(Sale sAmountQuota,1,0) OVER (ORDER BY CalendarYear, CalendarQuarter) AS Diff  
+FROM dbo.FactSalesQuota  
+WHERE EmployeeKey = 272 AND CalendarYear IN (2001,2002)  
+ORDER BY CalendarYear, CalendarQuarter; 
