9b5c120d10abd053f16e30a31ad86a7c66b10b21
naeem_hussien <naeem.hussien@bebit.com>
2020-04-11 23:55:17 +0900

    add percentile

--- sql/script.sql
+++ sql/script.sql
@@ -41,3 +41,29 @@ SELECT CalendarYear AS Year, CalendarQuarter AS Quarter, SalesAmountQuota AS Sal
 FROM dbo.FactSalesQuota  
 WHERE EmployeeKey = 272 AND CalendarYear IN (2001,2002)  
 ORDER BY CalendarYear, CalendarQuarter; 
+
+/* PERCENTILE_CONT */
+SELECT DISTINCT Name AS DepartmentName  
+      ,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianCont  
+      ,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianDisc  
+FROM HumanResources.Department AS d  
+INNER JOIN HumanResources.EmployeeDepartmentHistory AS dh   
+    ON dh.DepartmentID = d.DepartmentID  
+INNER JOIN HumanResources.EmployeePayHistory AS ph  
+    ON ph.BusinessEntityID = dh.BusinessEntityID  
+WHERE dh.EndDate IS NULL;
+
+/*PERCENTILE_DISC*/
+SELECT DISTINCT Name AS DepartmentName  
+      ,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianCont  
+      ,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianDisc  
+FROM HumanResources.Department AS d  
+INNER JOIN HumanResources.EmployeeDepartmentHistory AS dh   
+    ON dh.DepartmentID = d.DepartmentID  
+INNER JOIN HumanResources.EmployeePayHistory AS ph  
+    ON ph.BusinessEntityID = dh.BusinessEntityID  
+WHERE dh.EndDate IS NULL;
