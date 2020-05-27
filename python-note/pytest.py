a5a56db4c52182a3838023de6421bd4c974b995c
naeem_hussien <naeem.hussien@bebit.com>
2020-05-27 23:40:39 +0900

    pytest mark parameterize

--- python-note/pytest.py
+++ python-note/pytest.py
@@ -4,3 +4,13 @@ from unittest import mock
 @pytest.fixture
 def bob():
     return {"name": "Bob"}
+
+assert result
+
+@pytest.mark.parameterize('num1', 'num2', 'result'[
+    (1,2,10),
+    ("hello", "world", "hello world"),
+    (3,5,7)
+])
+def test_num(num1, num2, result):
+    assert file_name.function_name() == result
