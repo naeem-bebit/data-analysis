b83a59d68e46722b0b06ea8abe1b54e4c433440d
naeem_hussien <naeem.hussien@bebit.com>
2020-06-04 23:34:53 +0900

    CLI pytest

--- python-note/pytest.py
+++ python-note/pytest.py
@@ -34,3 +34,29 @@ pytest --cov=lambda_function --cov-report term-missing
 pytest integration test
 How to do integration test?
 pytest integrationtest.py
+
+let say we want to test it using command line
+# content of test_sample.py
+def test_answer(cmdopt):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
+    assert 0  # to see what was printed
+
+# content of conftest.py
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+    )
+
+
+@pytest.fixture
+def cmdopt(request):
+    return request.config.getoption("--cmdopt")
+
+#CLI to test
+pytest -q --cmdopt=type2
