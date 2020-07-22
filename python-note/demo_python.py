de5d5b12a8a80343b0e7e1e370e081b6a46c7d82
naeem_hussien <naeem.hussien@bebit.com>
2020-07-22 23:49:25 +0900

    global variable

--- python-note/demo_python.py
+++ python-note/demo_python.py
@@ -10,6 +10,7 @@ def f1():
 def a(f):
     f()
 
+
 a(f1)
 # f1()
 # print(f1)
@@ -18,14 +19,53 @@ a(f1)
 # Recursion
 
 import sys
-print(sys.getrecursionlimit()) #the limit is 1000
+
+print(sys.getrecursionlimit())  # the limit is 1000
 sys.setrecursionlimit(10)
 
 i = 0
+
+
 def hello():
-    global i # set the global i to 0
+    global i  # set the global i to 0, it will replace all the value with i =0
     print('Hello', i)
-    i = i+ 1
+    i = i + 1
     hello()
 
+
 # hello() # function calling itself
+
+
+def add(a, *b):  # b will be tuple
+    pass
+    # c = a+b
+    # print(c)
+
+
+print(add(1, 2))
+
+a = 0
+for i in (10, 20, 30):
+    # print(i)
+    a = i + a
+print(a)
+
+
+def hello_there(*a, **b):  # b is the value. a is the tuple
+    print(a)
+    print(b)
+    for i, j in b.items():
+        print(i, j)
+
+
+hello_there(1, 3, a=3, b=4)
+
+ab = 1234
+
+def some():
+    global ab
+    print(ab)
+
+    globals()['ab'] = 12 #change the value of the global variable to 12
+some()
+print(ab)
