a43058c8dc302874ad1f8f301a0d2892b46418af
naeem_hussien <naeem.hussien@bebit.com>
2020-07-23 23:38:05 +0900

    inheritance

--- python-note/demo_python.py
+++ python-note/demo_python.py
@@ -69,3 +69,54 @@ def some():
     globals()['ab'] = 12 #change the value of the global variable to 12
 some()
 print(ab)
+def some_func(a,b,c):
+    print(a,b,c)
+
+# Inheritance
+
+class ab:
+
+    def func1(self):
+        print('function 1')
+
+    def func2(self):
+        print('function 2')
+
+class abc:
+
+    def func3(self):
+        print('function 3')
+
+    def func4(self):
+        print('function 4')
+
+
+class abcd(ab, abc):
+
+    def func5(self):
+        print('function 5')
+
+ab_object = abcd()
+ab_object.func1()
+
+class a1:
+    def __init__(self):
+        print('test a1')
+
+a1 = a1()
+
+class a2:
+
+    def __init__(self, val1, val2):
+        self.val1 = val1
+        self.val2 = val2
+
+    def val_1(self):
+        print(self.val1)
+
+    def b2(self):
+        print('test a2')
+
+a2 = a2(12,23)
+a2.b2()
+a2.val_1()
