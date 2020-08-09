583cd32300c29f2ebd857a3d0801c5ca311f5fa6
naeem_hussien <naeem.hussien@bebit.com>
2020-08-09 20:23:18 +0900

    kwargs

--- python-note/a_demo.py
+++ python-note/a_demo.py
@@ -1,16 +1,19 @@
+import sys
+
 i = 0
 while i < 5:
     i += 1
     if i == 3:
         break
-    print('first', i)
+    print('first', i, end=' ')
 
 i = 0
 while i < 5:
     i += 1
     if i == 3:
         continue
-    print('second', i)
+    # print('second', i)
+    print()  # print new line
 
 i = 0
 while i < 5:
@@ -25,7 +28,7 @@ else:
 # i = 0
 for i in range(5):
     if i == 3:
-        print(i*3)
+        print(i * 3)
         continue
     print(i)
 else:
@@ -34,6 +37,34 @@ else:
 for i in range(3, 8, 2):
     print('fourth', i)
 
-times2 = lambda a: a*2
+times2 = lambda a: a * 2
 print('lam', times2(2))
 
+# x = int(sys.argv[1]) # argv for the terminal
+# y = int(sys.argv[2])
+# print(x+y)
+
+for i in range(4):
+    for i in range(4):
+        print('# ', end='')
+    # print('\n')
+    print()  # different with print new line
+
+for i in range(4):
+    for ix in range(1, i + 2):
+        print(ix, end='')
+    print()
+
+for i in range(4):
+    for ix in range(4 - i):
+        print(ix, end='')
+    print()
+
+
+def add(x, y):
+    c = x + y
+    return c
+
+result = add(2,3)
+print(result)
+print(add(1, 2))
