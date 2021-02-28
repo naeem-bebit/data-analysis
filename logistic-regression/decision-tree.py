e11d325afc5c41b5fa68709782615636b00e030b
naeem-bebit <naeem@naeems-MacBook-Pro.local>
2021-02-28 16:49:32 +0800

    revise

--- logistic-regression/decision-tree.py
+++ logistic-regression/decision-tree.py
@@ -1,18 +1,21 @@
 # Decision Tree Classification
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
 
-# Importing the dataset
+
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=0
 )
@@ -22,8 +25,6 @@ print(X_test)
 print(y_test)
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
@@ -31,8 +32,6 @@ print(X_train)
 print(X_test)
 
 # Training the Decision Tree Classification model on the Training set
-from sklearn.tree import DecisionTreeClassifier
-
 classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
 classifier.fit(X_train, y_train)
 
@@ -46,15 +45,11 @@ print(
 )
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
-
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
 # Visualising the Training set results
-from matplotlib.colors import ListedColormap
-
 X_set, y_set = sc.inverse_transform(X_train), y_train
 X1, X2 = np.meshgrid(
     np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
@@ -85,7 +80,6 @@ plt.legend()
 plt.show()
 
 # Visualising the Test set results
-from matplotlib.colors import ListedColormap
 
 X_set, y_set = sc.inverse_transform(X_test), y_test
 X1, X2 = np.meshgrid(
