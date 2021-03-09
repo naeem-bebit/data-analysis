e77d8d8fd15a8d7bcb0299d524df305fdcf8ffee
naeem-bebit <naeemhussien@gmail.com>
2021-03-09 21:07:58 +0800

    fix bs4

--- python-note/webscrapper.py
+++ python-note/webscrapper.py
@@ -1,21 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 url = 'https://www.lelong.com.my/catalog/all/list?TheKeyword=macbook+pro&D='
 
 # write a loop to scrape from page 1 to the last page
+first_page = 1
+last_page = 2
 
-product_name=[]
-for page in range(1,19):
-url_page = url+str(page)
-scrape = requests.get(url_page)
-soup = BeautifulSoup(scrape.content, 'lxml')
-link = soup.find_all('div',{'class':'item','class':'summary'})
-length = len(link)
+for page in range(first_page,last_page):
+    url_page = url+str(page)
+    scrape = requests.get(url_page)
+    soup = BeautifulSoup(scrape.content, 'lxml')
+    link = soup.find_all('div',{'class':'item','class':'summary'})
+    print(link)
+    length = len(link)
+
+product_names=[]
 for i in range(0,length):
-name = link[i].a.get('title')
-product_name.append(name)
+    product_title = link[i].a.get('title')
+    product_names.append(product_title)
 
 # write to csv
 # convert the list to a pandas dataframe
-
-df = pd.DataFrame({'name':product_name})
-df
-df.to_csv('output.csv', index=False)
+df = pd.DataFrame({'name':product_names})
+df.to_csv('scrapped_data.csv', index=False)
