39cb9a5d7593e6536f972bf3326e979c39cc2f12
naeem-bebit <naeemhussien@gmail.com>
2021-03-12 22:36:32 +0800

    price append

--- python-note/webscrapper.py
+++ python-note/webscrapper.py
@@ -7,33 +7,32 @@ url = 'https://www.lelong.com.my/catalog/all/list?TheKeyword=macbook+pro&D='
 first_page = 1
 last_page = 2
 
-# for page in range(first_page,last_page):
-#     url_page = url+str(page)
-#     scrape = requests.get(url_page)
-#     soup = BeautifulSoup(scrape.content, 'lxml')
-#     link = soup.find_all('div',{'class':'item','class':'summary', 'class':'price-section'})
-#     print(link)
-#     length = len(link)
+for page in range(first_page,last_page):
+    url_page = url+str(page)
+    scrape = requests.get(url_page)
+    soup = BeautifulSoup(scrape.content, 'lxml')
+    link = soup.find_all('div',{'class':'item','class':'summary'})
+    length = len(link)
 
-scrape = requests.get('https://www.lelong.com.my/catalog/all/list?TheKeyword=macbook+pro&D=1')
-soup = BeautifulSoup(scrape.content, 'lxml')
-# link = soup.find_all('div',{'class':'item','class':'summary', 'class':'price-section'})
-link = soup.find_all('div',{'class':'item','class':'summary'})
+# scrape = requests.get('https://www.lelong.com.my/catalog/all/list?TheKeyword=macbook+pro&D=1')
+# soup = BeautifulSoup(scrape.content, 'lxml')
+# # link = soup.find_all('div',{'class':'item','class':'summary', 'class':'price-section'})
+# link = soup.find_all('div',{'class':'item','class':'summary'})
 
-print(link)
-length = len(link)
-print("LENGTH: ", length)
-product_title = link[1].a.b.get('data-price')
-# product_title = link[1].a.get('discountpercentage')
-print(product_title)
+# print(link)
+# length = len(link)
+# print("LENGTH: ", length)
+# product_title = link[1].a.b.get('data-price')
+# # product_title = link[1].a.get('discountpercentage')
+# print(product_title)
 
 
-# product_names=[]
-# for i in range(0,length):
-#     product_title = link[i].a.get('discountpercentage')
-#     product_names.append(product_title)
+product_names=[]
+for i in range(0,length):
+    product_title = link[i].a.b.get('discountpercentage')
+    product_names.append(product_title)
 
 # write to csv
 # convert the list to a pandas dataframe
-# df = pd.DataFrame({'name':product_names})
+df = pd.DataFrame({'name':product_names})
 # df.to_csv('scrapped_data.csv', index=False)
