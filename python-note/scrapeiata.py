99827342e72cf86594b31a8b6db02b90d4bc5b9c
naeem-bebit <naeemhussien@gmail.com>
2021-04-22 17:42:53 +0800

    tripadvisor

--- python-note/scrapeiata.py
+++ python-note/scrapeiata.py
@@ -8,6 +8,7 @@ s = requests.Session()
 response = s.get(url, timeout=10)
 soup = BeautifulSoup(response.content, 'html.parser')
 pretty_soup = soup.prettify()
+# print(pretty_soup)
 print(soup.title.string)
 # all_tables=soup.find_all('table')
 # print(all_tables)
@@ -19,10 +20,38 @@ for row in wiki_table.findAll("tr")[1:]:
 #     print(row.text.rstrip()) #focus on rstrip
     if len(row) == 12:
         iata_rowA.append(row.text.rstrip())
-    elif len(row) == 2: 
+    else:
         continue
 
 df_iataA = pd.DataFrame(iata_rowA,columns = ['a'])
 # iata_rowA
 df_iataA = df_iataA['a'].str.split('\n',expand=True)
-print(df_iataA.head()
+print(df_iataA.head())
+
+#----
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://www.tripadvisor.in/Hotels-g187147-Paris_Ile_de_France-Hotels.html'
+print(url)
+s = requests.Session()
+response = s.get(url, timeout=10)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+ratings = []
+for rating in soup.find_all('a',{'class':'ui_bubble_rating'}):
+    ratings.append(rating['alt'])
+
+hotel = []
+for name in soup.findAll('div',{'class':'listing_title'}):
+    hotel.append(name.text.strip())
+
+price = []
+
+for p in soup.findAll('div',{'class':'price-wrap'}):
+    price.append(p.text) 
+price[:5]
+
+pd.DataFrame(hotel)
