2d65b90d8c31e4bb55e276825f624167d46a6e6a
naeem-bebit <naeemhussien@gmail.com>
2021-06-17 23:43:22 +0800

    cur.fetchall

--- python-note/databasesqlite.py
+++ python-note/databasesqlite.py
@@ -1,7 +1,13 @@
 import sqlite3
+import pandas as pd
 
-db = sqlite3.connect('book.db')
+db = sqlite3.connect('books.db')
+df = pd.read_sql_query('SELECT * FROM books;',db)
 
+new_data = {'id':'12','author': 'Naeem'}
+df = df.append(new_data, ignore_index=True)
+
+data.to_sql('books', db, if_exists = 'replace', index = False)
 cur = db.cursor()
 
 cur.execute('''()''')
@@ -9,6 +15,9 @@ cur.execute('''()''')
 cur.executemany()
 
 cur.execute('SELECT * FROM books')
+
+for new_line in cur.fetchall()
+    print(new_line)
 
 print(cur.fetchall)
 
