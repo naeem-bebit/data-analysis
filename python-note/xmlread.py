d3c27243f31401f22705d7a6c21bf18acd10b6b0
naeem-bebit <naeemhussien@gmail.com>
2021-06-29 23:19:51 +0800

    diff

--- python-note/xmlread.py
+++ python-note/xmlread.py
@@ -1,6 +1,7 @@
 import boto3
 import xml.etree.ElementTree as ET
 from datetime import datetime
+import pandas as pd
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('future-markets-prod-s3-datalakebucket')
@@ -23,10 +24,22 @@ for obj in bucket.objects.filter(Prefix='Malaysia/Bookings/2021-06-21/QRH002936_
             print(ch.tag, ch.attrib)
         print(child.tag, child.attrib)
 
-pd.date_range(start="2021-02-01",end="2021-02-05"):
+for i in pd.date_range(start="2021-02-01",end="2021-02-05"):
+    print(i)
 
 (datetime.now() - pd.to_datetime(date_test)) < timedelta(minutes=15)
 if (datetime.now() - pd.to_datetime(date_test)) < timedelta(minutes=15):
     print('Yes')
 else:
     print('No')
+
+# Check two files for difference
+with open('QRH002934_20210616_065718 2.xml', 'r') as file1:
+    with open('QRH002934_20210613_214107 2.xml', 'r') as file2:
+        difference = set(file1).difference(file2)
+        print(difference)
+difference.discard('\n')
+
+with open('diff.txt', 'w') as file_out:
+    for line in difference:
+        file_out.write(line)
