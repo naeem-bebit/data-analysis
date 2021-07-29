85e8bdb39e9702f922f0f6b023c9709a71cf0070
naeem-bebit <naeemhussien@gmail.com>
2021-07-29 23:45:18 +0800

    title

--- python-note/carlistbs4.py
+++ python-note/carlistbs4.py
@@ -1,28 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-# url = 'https://www.carlist.my/cars-for-sale/malaysia?inspected=true'
 
+page_range = 2
+
+# try:
+#     this
+# else:
+#     exit
+url = f'https://www.carlist.my/cars-for-sale/malaysia?inspected=true&page_number={page_range}&page_size=25'
 # # write a loop to scrape from page 1 to the last page
 # first_page = 1
 # last_page = 2
 
-# for page in range(first_page,last_page):
-#     url_page = url+str(page)
-#     scrape = requests.get(url_page)
-#     soup = BeautifulSoup(scrape.content, 'lxml')
-#     link = soup.find_all('div',{'class':'item','class':'summary'})
-#     # print(link)
-#     length = len(link)
-
-url = 'https://www.skysports.com/premier-league-table'
-
-r = requests.get(url)
-print(r.status_code)
-soup = BeautifulSoup(r.text, 'html.parser')
-
-# epl_table = soup.find('table', class_ = 'standing-table__table')
-epl_table = soup.find('table', {'class':'standing-table__table'})
-
-print(epl_table)
-
+for page in range(page_range):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    car = soup.find('article', class_ = 'listing')
+    # print(car)
+    print(car.get('data-title'))
+#     epl_table = soup.find('table', class_ = 'standing-table__table')
+    # epl_table = soup.find('table', {'class':'standing-table__table'})
+    # print(url_page)
+    # length = len(link)
