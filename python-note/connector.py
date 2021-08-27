dc1f8f2b064f7cd3e190a5b401b30090ed798b81
naeem-bebit <naeemhussien@gmail.com>
2021-08-27 15:25:38 +0800

    pd.sql

--- python-note/connector.py
+++ python-note/connector.py
@@ -3,26 +3,53 @@
 
 import psycopg2
 
+table_name = 'rnc_database'
+
 try:
- connection = psycopg2.connect(user="reader",
-                               password="NWDMCE5xdipIjRrp",
-                               host="hh-pgsql-public.ebi.ac.uk",
-                            #    port="5432", Optional
-                               database="pfmegrnargs")
+    connection = psycopg2.connect(user="reader",
+                                  password="NWDMCE5xdipIjRrp",
+                                  host="hh-pgsql-public.ebi.ac.uk",
+#                                   port="5432", #Optional
+                                  database="pfmegrnargs")
 
- cursor = connection.cursor()
-
- cursor.execute(f"SELECT * FROM rnc_database limit 10;")
- record = cursor.fetchall()
- print(record, "\n")
+    cursor = connection.cursor()
+    sqlquery = f"SELECT * FROM {table_name} limit 10;"
+    cursor.execute(sqlquery)
+    record = cursor.fetchall()
+    print(record, "\n")
 
 except (Exception, psycopg2.Error) as error:
- print("Error while connecting to PostgreSQL", error)
+    print("Error while connecting to PostgreSQL", error)
 finally:
- try:
-     if connection:
-         cursor.close()
-         connection.close()
-         print("PostgreSQL connection is closed")
- except:
-     print("Connection timeout")
+    try:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    except:
+        print("Connection timeout")
+
+
+import psycopg2
+import pandas as pd
+
+try:
+    connection = psycopg2.connect(user="reader",
+                                  password="NWDMCE5xdipIjRrp",
+                                  host="hh-pgsql-public.ebi.ac.uk",
+#                                   port="5432", #Optional
+                                  database="pfmegrnargs")
+    
+    sqlquery = 'SELECT * FROM rnc_database;'
+    df_sql = pd.read_sql(sqlquery, connection)
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    try:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    except:
+        print("Connection timeout")
---
+++ python-note/jupyter_header.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+pd.set_option('display.max_columns', None)
