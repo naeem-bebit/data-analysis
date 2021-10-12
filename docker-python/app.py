daf3766104de616d1a1b33bf1ac80bb3e7520cdb
naeem-bebit <naeemhussien@gmail.com>
2021-10-12 23:47:40 +0800

    add app.py

--- docker-python/app.py
+++ docker-python/app.py
@@ -1,6 +1,23 @@
-import numpy as np
+import time
 
-array_1 = np.array([1, 2, 3])
-print(array_1)
-print("hello Naeem")
-print('new docker image 3')
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
