d78df8e4a5f4ffabf78330d8125dba36ce1c1386
naeem-bebit <naeemhussien@gmail.com>
2021-11-10 23:40:31 +0800

    import shap

--- python-note/shap.py
+++ python-note/shap.py
@@ -32,6 +32,7 @@ shap.force_plot(k_explainer.expected_value[1], k_shap_values[1], data_for_predic
 
 
 #advance SHAP
+# https://www.kaggle.com/dansbecker/advanced-uses-of-shap-values
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -43,3 +44,15 @@ feature_names = [i for i in data.columns if data[i].dtype in [np.int64, np.int64
 X = data[feature_names]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 my_model = RandomForestClassifier(random_state=0).fit(train_X, train_y)
+
+import shap  # package used to calculate Shap values
+
+# Create object that can calculate shap values
+explainer = shap.TreeExplainer(my_model)
+
+# calculate shap values. This is what we will plot.
+# Calculate shap_values for all of val_X rather than a single row, to have more data for plot.
+shap_values = explainer.shap_values(val_X)
+
+# Make plot. Index of [1] is explained in text below.
+shap.summary_plot(shap_values[1], val_X)
