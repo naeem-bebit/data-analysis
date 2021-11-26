aed9d31430cbb0850b31444a7c12bbfbf39cae06
Naeem <52814859+naeem-bebit@users.noreply.github.com>
2021-11-26 19:28:04 +0800

    remove sample code

--- python-note/py-note.py
+++ python-note/py-note.py
@@ -1,5 +1,3 @@
-"""Reference for Python code examples."""
-
 # Dictionary True
 n = int(input().strip())
 check = {True: "Not Weird", False: "Weird"}
@@ -10,31 +8,6 @@ for i in range(1, (n + 1)):
     print(i, end="")
 
 int("".join(map(str, range(1, (int(input())+1)))))
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-if __name__ == '__main__':
-    app.run()
 
 # ---- data modelling
 df = pd.read_csv('YoutubeSpamMergedData.csv')
