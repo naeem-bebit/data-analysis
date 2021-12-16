7734a0aa8939692974c70f59da91f7050b020f1a
naeem-bebit <naeemhussien@gmail.com>
2021-12-16 14:26:20 +0800

    revise tempfile

--- python-note/temp_file.py
+++ python-note/temp_file.py
@@ -13,6 +13,20 @@ print(temp.name)
 temp = tempfile.NamedTemporaryFile(prefix='pre_', suffix='_suf')
 print(temp.name)
 
+temp = tempfile.TemporaryFile()
+temp.write(b'foo bar')
+temp.seek(0)
+print(temp.read())
+temp.close()
+
+temp_dir = tempfile.TemporaryDirectory()
+print(temp_dir)
+
+secure_temp = tempfile.mkstemp(prefix="pre_",suffix="_suf")
+print(secure_temp)
+  
+tempfile.tempdir = "/temp"
+print(tempfile.gettempdir())
 
 with tempfile.TemporaryFile() as fp:
     fp.write(b'Hello world!')
