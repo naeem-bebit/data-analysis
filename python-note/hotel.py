9c253351e0bce26c68a3d2d9e337143fd36ebddb
naeem-bebit <naeemhussien@gmail.com>
2022-01-20 10:22:34 +0800

    feature importance

--- python-note/hotel.py
+++ python-note/hotel.py
@@ -307,3 +307,41 @@ best_summary = pd.DataFrame({
     'score': score_list
 })
 print(best_summary)
+
+## Feature importance
+
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+X, y = make_classification(
+    n_samples=1000,
+    n_features=10,
+    n_informative=3,
+    n_redundant=0,
+    n_repeated=0,
+    n_classes=2,
+    random_state=0,
+    shuffle=False,
+)
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+feature_names = [f"feature {i}" for i in range(X.shape[1])]
+forest = RandomForestClassifier(random_state=0)
+forest.fit(X_train, y_train)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+forest_importances = pd.Series(importances, index=feature_names)
+
+fig, ax = plt.subplots()
+forest_importances.plot.bar(yerr=std, ax=ax)
+ax.set_title("Feature importances using MDI")
+ax.set_ylabel("Mean decrease in impurity")
+fig.tight_layout()
+
+feature_names
+
+forest_importances.sort_values(ascending=False)
