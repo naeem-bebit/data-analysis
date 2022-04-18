c37a7e427c9faeea60dd6ec5cced8624a067df46
naeem-bebit <naeemhussien@gmail.com>
2022-04-18 23:29:01 +0800

    pdfminer3

--- python-note/pdfreader.py
+++ python-note/pdfreader.py
@@ -45,3 +45,34 @@ def extract_information(pdf_path):
 if __name__ == '__main__':
     path = 'sample2.pdf'
     information = extract_information(path)
+
+#pdfminer library focuses entirely on getting and analyzing text data
+# The result more refine text extraction
+
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import PDFPageAggregator
+from pdfminer3.converter import TextConverter
+import io
+
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+with open('80-56-17801-512G_SDCZ430_G46_specs.pdf', 'rb') as fh:
+
+    for page in PDFPage.get_pages(fh,
+                                  caching=True,
+                                  check_extractable=True):
+        page_interpreter.process_page(page)
+
+    text = fake_file_handle.getvalue()
+
+# close open handles
+converter.close()
+fake_file_handle.close()
+
+print(text)
