08f7946475a4de3c5cf34d78f45900d8b057fbe2
naeem-bebit <naeemhussien@gmail.com>
2022-05-13 23:44:02 +0800

    logger config

--- python-note/logging_py.py
+++ python-note/logging_py.py
@@ -1,11 +1,11 @@
 import logging
+import pandas as pd
 
 # logging.basicConfig(level=logging.DEBUG, filename="test.log", filemode="w",
 #                     format="%(asctime)s - %(levelname)s - %(message)s")
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-# logging.basicConfig(level=logging.INFO, filemode="w")
-
 handler = logging.FileHandler('test.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -31,3 +31,11 @@ except NameError:
 # logging.warning("warning")
 # logging.error("error")
 # logging.critical("critical")
+
+df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
+                   'num_wings': [2, 0, 0, 0],
+                   'num_specimen_seen': [10, 2, 1, 8]},
+                  index=['falcon', 'dog', 'spider', 'fish'])
+# logger.info(df)
+logger.info(f"\n {df}")#.to_string().replace('\n', '\n\t'))
+logger.info(f"preprocess_blade \n {df}")
