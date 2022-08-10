3d13b354ce5de0b3fc63b1f98cbed77fee63a6ed
naeem-bebit <naeemhussien@gmail.com>
2022-08-10 23:56:45 +0800

    prediction

--- python-note/detectron.py
+++ python-note/detectron.py
@@ -17,3 +17,15 @@ setup_logger()
 !wget http: // images.cocodataset.org/val2017/000000439715.jpg - q - O input.jpg
 im = cv2.imread("./input.jpg")
 cv2_imshow(im)
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+v = Visualizer(
+    im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2_imshow(out.get_image()[:, :, ::-1])
