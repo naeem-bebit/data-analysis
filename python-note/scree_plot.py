31004e8f47df70378a6ee6a34475842733cd20d8
naeem-bebit <naeemhussien@gmail.com>
2022-12-05 17:29:08 +0800

    scree plot

---
+++ python-note/scree_plot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+url = "https://raw.githubusercontent.com/JWarmenhoven/ISLR-python/master/Notebooks/Data/USArrests.csv"
+data = pd.read_csv(url)
+df = data.iloc[:, 1:5]
+scaler = StandardScaler()
+scaled_df = df.copy()
+scaled_df = pd.DataFrame(scaler.fit_transform(
+    scaled_df), columns=scaled_df.columns)
