1cec7da048c8a4c905641d8b82dd3d0713389c28
naeem-bebit <naeemhussien@gmail.com>
2022-12-06 11:10:10 +0800

    PCA 4 components

--- python-note/scree_plot.py
+++ python-note/scree_plot.py
@@ -1,5 +1,7 @@
+from sklearn.decomposition import PCA
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+
 url = "https://raw.githubusercontent.com/JWarmenhoven/ISLR-python/master/Notebooks/Data/USArrests.csv"
 data = pd.read_csv(url)
 df = data.iloc[:, 1:5]
@@ -7,3 +9,6 @@ scaler = StandardScaler()
 scaled_df = df.copy()
 scaled_df = pd.DataFrame(scaler.fit_transform(
     scaled_df), columns=scaled_df.columns)
+
+pca = PCA(n_components=4)
+pca_fit = pca.fit(scaled_df)
