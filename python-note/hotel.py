437e0757f12836d60e93b9fd5b6653a0815b9960
naeem-bebit <naeemhussien@gmail.com>
2023-01-17 15:15:56 +0800

    compare two files

--- python-note/hotel.py
+++ python-note/hotel.py
@@ -1492,3 +1492,13 @@ if __name__ == "__main__":
 # from zipfile import ZipFile
 with ZipFile("PDF for Global\P001-003300-240G.zip", "r") as zip_ref:
     zip_ref.extractall("PDF for Global/target_dir")
+
+# compare two csv files
+with open('test1.csv', 'r') as t1, open('test2.csv', 'r') as t2:
+    fileone=t1.readlines()
+    filetwo=t2.readlines()
+
+with open('update.csv', 'w') as outFile:
+    for line in filetwo:
+        if line not in fileone:
+            outFile.write(line)
