3f5c0712ef3cd07ed5af68ad24c086077e737cb9
naeem-bebit <naeemhussien@gmail.com>
2023-03-17 15:38:13 +0800

    pdf extraction

---
+++ python-note/extract_pdf.py
@@ -0,0 +1,23 @@
+import io
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+import io
+
+output_string = io.StringIO()
+with open('invoices/InvoiceINV1006.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+text = output_string.getvalue()
+print(text)
+output_string.close()
+device.close()
